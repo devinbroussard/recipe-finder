ad0848da170c53e21c77637dcb7311cd0bddb9ad
taylorwaddell <Taylor.Waddell@verisglobal.com>
2023-09-07 18:08:00 -0500

    Removed samplers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import "./App.css";
 
-import Button from "./components/Button/Button";
-
 function App() {
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <Button label="button" variant={"primary"} />
-      <Button label="button" variant={"danger"} />
-      <Button label="button" variant={"light"} />
-      <Button label="button" variant={"dark"} />
-      <Button label="" variant={"icon"} />
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
