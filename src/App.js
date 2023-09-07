02e56e21ce7c0ba70ddb2d36fae8493ab9aea590
taylorwaddell <Taylor.Waddell@verisglobal.com>
2023-09-07 18:04:42 -0500

    created variables file and laid button foundation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import "./App.css";
+
+import Button from "./components/Button/Button";
 
 function App() {
   return (
-   <></>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Button label="button" variant={"primary"} />
+      <Button label="button" variant={"danger"} />
+      <Button label="button" variant={"light"} />
+      <Button label="button" variant={"dark"} />
+      <Button label="" variant={"icon"} />
+    </div>
   );
 }
 
